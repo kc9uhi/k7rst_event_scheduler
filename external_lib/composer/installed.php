a12cc0e2b8ff1bb8d72ad91372deb0e0a61d9618
kc9uhi <kc9uhi@gmail.com>
2026-02-19 20:45:02 -0700

    add Brick\Geo

---
+++ external_lib/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4b8032f70015944e900bf1502486e3982f87cd06',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4b8032f70015944e900bf1502486e3982f87cd06',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'brick/geo' => array(
+            'pretty_version' => '0.13.1',
+            'version' => '0.13.1.0',
+            'reference' => '723b6a2f6b97fcb977b2f3cb91a1806dfbef250a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../brick/geo',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
